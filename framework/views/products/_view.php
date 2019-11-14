6b71413cb5644e6350a6e379a380af1642ae4e80
evgeniyc <evgeniychp@i.ua>
2019-11-14 17:30:41 +0200

    thuesday js good work

--- framework/views/products/_view.php
+++ framework/views/products/_view.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 use yii\web\View;
+use rmrevin\yii\fontawesome\FAS;
 /* @var $this yii\web\View */
 /* @var $model app\models\Products */
 
@@ -10,21 +11,31 @@ use yii\web\View;
 //$this->params['breadcrumbs'][] = ['label' => 'Products', 'url' => ['index']];
 //$this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
-$this->registerJs( "$('.item').on('click', 	function() { 
-												if( $(this).find('.products-descr').css('display') != 'none') {
-													$(this).find('.products-descr').slideToggle('slow'); 
+//rmrevin\yii\fontawesome\AssetBundle::register($this);
+
+$this->registerJs( "$('.fa-list').on('click', 	function() { 
+												//var descr = $(this).closest('.products-descr');
+												var item = $(this).closest('.item');
+												var descr = item.find('.products-descr');
+												if( descr.css('display') != 'none') {
+													descr.slideToggle('slow'); 
 													return false;
 												}
 												$('.item').css('z-index',1);
 												$('.products-descr').css('display','none');
-												$(this).css('z-index', 3); 
-												$(this).find('.products-descr').slideToggle('slow'); 
+												item.css('z-index', 3); 
+												descr.slideToggle('slow'); 
 											})");
+$this->registerJs( "$('.products-descr').on('click', 	function() { 
+												$(this).slideToggle('slow'); 
+											})");
+$this->registerJs( "$('.fa-list').attr('title', 'Описание');$('.fa-eye').attr('title', 'Обзор');$('.fa-shopping-basket').attr('title', 'Купить');");
 //$this->registerJs( "$('.products-view').on('click', function() { var css = $(this).css('z-index'); alert('css: '+css)})");
 ?>
 <div class="products-view">
 	<div class="products-img"><?= Html::img('@web/uploads/'.$model->img, ['alt' => $model->title]) ?></div>
 	<div class="products-title"><?= Html::encode($model->title) ?></div>
-	<div class="products-price">Цена: <?= $model->price ?> грн.<i class="fas fa-camera"></i></div>
+	<div class ="products-icons"><?= FAS::icon('list');?>&nbsp;&nbsp;<?=FAS::icon('eye');?>&nbsp;&nbsp;<?=FAS::icon('shopping-basket');?></div>
+	<div class="products-price">Цена: <?= $model->price ?> грн.</div>
 	<div class="products-descr"><?= Html::encode($model->descr) ?></div>
 </div>
